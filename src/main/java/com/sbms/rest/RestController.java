6c41f3209fc9cfb443181e86e3dd159a3ee111bf
SatyaEnubothu <satyamcajava@gmail.com>
2021-12-05 18:06:47 +0530

    WH 999- Initial

---
+++ src/main/java/com/sbms/rest/RestController.java
@@ -0,0 +1,14 @@
+package com.sbms.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+	@GetMapping("/welcome")
+	public String getMsg() {
+		String msg = "Welcome to Spring Boot Security";
+		return msg;
+	}
+
+}
