38df9c4304dd687043b4e1c524c73c6e4c3426e9
SatyaEnubothu <satyamcajava@gmail.com>
2022-03-12 21:29:56 +0530

    wish method added

--- src/main/java/com/sbms/rest/RestController.java
+++ src/main/java/com/sbms/rest/RestController.java
@@ -1,5 +1,9 @@
 package com.sbms.rest;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
@@ -7,8 +11,22 @@ public class RestController {
 
 	@GetMapping("/welcome")
 	public String getMsg() {
+		LocalDateTime ldt = LocalDateTime.now();
+		int hour = ldt.getHour();
+		if (hour < 12)
+			return "Good Morning";
+		else if (hour < 16)
+			return "Good Afternoon";
+		else if (hour < 20)
+			return "Good Evening";
+		else
+			return "Good Night";
+	}
+
+	@GetMapping("/wish")
+	public ResponseEntity<String> wishmsg() {
 		String msg = "Welcome to Spring Boot Security with Dockers";
-		return msg;
+		return new ResponseEntity<String>(msg, HttpStatus.OK);
 	}
 
 }
